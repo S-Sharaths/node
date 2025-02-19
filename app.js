9070527798c7992f93580da0b7951bedb5f7f107
S-Sharaths <sharathprogramming4@gmail.com>
2025-02-19 16:13:48 +0530

    after exporting it as object and importing

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 require("./xyz.js");
 
+const calculateSum = require("./sum.js");
+
 var name = "hi node";
 
 var a = 10;
 var b = 20;
+
+calculateSum(a, b);
 
 // console.log(name);
 // console.log(a + b);
---
+++ sum.js
@@ -0,0 +1,11 @@
+var x = "hello world";
+
+function calculateSum(a, b) {
+  const sum = a + b;
+  console.log(sum);
+}
+
+module.exports = {
+  x: x,
+  calculateSum: calculateSum,
+};
