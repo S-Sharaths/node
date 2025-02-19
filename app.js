6e466d9ba9bb4537cb2f0b31bb7f0c4e80a0e259
S-Sharaths <sharathprogramming4@gmail.com>
2025-02-19 16:29:36 +0530

    object short end

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 require("./xyz.js");
 
-const calculateSum = require("./sum.js");
+const calculateObject = require("./sum.js");
 
 var name = "hi node";
 
 var a = 10;
 var b = 20;
+calculateObject.calculateSum(a, b);
 
-calculateSum(a, b);
-
+console.log(calculateObject.x);
 // console.log(name);
 // console.log(a + b);
 // console.log(global);
--- sum.js
+++ sum.js
@@ -6,6 +6,6 @@ function calculateSum(a, b) {
 }
 
 module.exports = {
-  x: x,
-  calculateSum: calculateSum,
+  x,
+  calculateSum,
 };
